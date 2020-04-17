07003aa57ece40a5011bd19185afe92170e9de83
James Carter <jlcarter.2342@outlook.com>
2020-04-17 14:51:33 -0500

    Added a library for the data access layer to get user data from Db

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserbyId(userId);
+        }
+    }
+}
